97a7823830ebce0a8f41b882c9b978c364378601
Mirza Younus Baig <mirzayounus40@gmail.com>
2023-03-25 00:27:25 -0400

    Updated solution with optimal time complexity

--- Best Time to Buy Stock.py
+++ Best Time to Buy Stock.py
@@ -2,16 +2,25 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxProfit = 0
-        for i in range(len(prices)):
-            maxn = max(prices[i:]) - prices[i]
-            if maxProfit < maxn:
-                maxProfit = maxn
+        # Brute force solution
+        # maxProfit = 0
+        # for i in range(len(prices)):
+        #     maxn = max(prices[i:]) - prices[i]
+        #     if maxProfit < maxn:
+        #         maxProfit = maxn
+
+        # return maxProfit
+        l, r = 0, 1
+        maxp = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxp = max(maxp, profit)
+            else:
+                l = r
             
-
-
-        return maxProfit
-
+            r += 1
+        return maxp
 
 res = Solution()
 
