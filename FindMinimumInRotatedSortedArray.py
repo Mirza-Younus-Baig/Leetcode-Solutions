f90131174b1916ebdb7931088ab8aa50540007c4
Mirza Younus Baig <mirzayounus40@gmail.com>
2023-04-14 20:52:29 -0400

    Solution

---
+++ FindMinimumInRotatedSortedArray.py
@@ -0,0 +1,33 @@
+
+
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+        
+            if nums[low] <= nums[high]:
+                return nums[low]
+        
+            mid = (low + high)//2
+
+            if nums[low] > nums[mid]:
+                high = mid
+            else:
+                low = mid + 1
+
+    
+
+if __name__ == '__main__':
+    # inpArray = list(map(int, input().split()))
+
+    # inpArray = [0,1,2,4,5,6,7]
+    inpArray = [5,6,7,-88, 0,1,2,4]
+    # inpArray = [7,0,1,2,4,5,6]
+
+    sol = Solution()
+    print(sol.findMin(inpArray))
