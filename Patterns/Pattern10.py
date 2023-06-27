92462e06f8de598ce7e5652edb8a5d220d190fa7
Mirza Younus Baig <mirzayounus40@gmail.com>
2023-06-26 20:32:52 -0400

    Pattern 10 implementation

---
+++ Patterns/Pattern10.py
@@ -0,0 +1,22 @@
+'''
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+'''
+
+n = int(input("Enter the number of rows "))
+
+for i in range(1, 2 * n):
+    if i <= n:
+        for j in range(i):
+            print("*", end = " ")
+    else:
+        for j in range(2*n - i):
+            print("*", end = " ")
+    print()
