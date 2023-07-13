aa71b17e9b466d5ba36b0b68b266b533a2b7d963
Mirza Younus Baig <mirzayounus40@gmail.com>
2023-07-13 15:59:19 -0400

    Selection Sort Implementation

---
+++ Sort/SelectionSort.py
@@ -0,0 +1,11 @@
+# arr = list(map(int, input().split()))
+arr = [12, 13, 5, 7, 9, 1]
+
+for i in range(len(arr) - 1):
+    minInd = i
+    for j in range(i , len(arr)):
+        if arr[j] < arr[minInd]:
+            minInd = j
+    arr[i], arr[minInd] = arr[minInd], arr[i]
+
+print(arr)
