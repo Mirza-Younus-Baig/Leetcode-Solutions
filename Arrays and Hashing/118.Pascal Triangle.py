744c98223e69a75e36e493eb43945e84b87621f0
Mirza Younus Baig <mirzayounus40@gmail.com>
2023-10-20 04:04:58 -0400

    Pascal Triangle:

--- Arrays and Hashing/118.Pascal Triangle.py
+++ Arrays and Hashing/118.Pascal Triangle.py
@@ -6,30 +6,45 @@
 '''
 
 from typing import List
+from math import ceil 
 
 class Solution:
     def elementAtPos(self, row: int, col: int) -> int:
         # rowCcol (nCr)
         res = 1
-        for i in range(col):
-            res = res * (row - i)
+        for i in range(col - 1):
+            res = res * (row - i - 1)
             res = res / (i+1)
-            # row -= 1e
         return res
-        pass
         
- 
     def printRow(self, row: int) -> List[int]:
+        # TC: O(n2)
+        # res = [1]
+        # if row < 2:
+        #     return res
+        # for i in range(2, row):
+        #     res.append(self.elementAtPos(row - 1, i - 1))
+        # res.append(1)
+        # return res
+
+        # Optimal Solution
+        # TC: O(n)
         res = [1]
-        if row < 2:
-            return res
-        for i in range(2, row):
-            res.append(self.elementAtPos(row - 1, i - 1))
-        res.append(1)
+        ans = 1
+        for i in range(1, row):
+            ans *= (row - i) 
+            ans //= i
+            res.append(ans)
+        
         return res
+
     
     def generate(self, numRows: int) -> List[List[int]]:
-        pass
+        ans = []
+        for i in range(1, numRows + 1):
+            ans.append(self.printRow(i))
+        
+        return ans
 
 
 
@@ -39,13 +54,13 @@ if __name__ == "__main__":
     # Problem 1
     row = 5
     col = 3
-    print(obj.elementAtPos(row - 1, col - 1))
+    print(obj.elementAtPos(row, col))
 
 
     # Problem 2
-    row = 6
+    row = 12
     print(obj.printRow(row))
 
     # # Problem 3
-    # n = 6
-    # print(obj.generate(n))
+    n = 6
+    print(obj.generate(n))
