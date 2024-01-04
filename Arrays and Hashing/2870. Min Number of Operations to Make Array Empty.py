115239e838d1ad1c00004167afd9f8e0fed25a53
Mirza Younus Baig <mirzayounus40@gmail.com>
2024-01-04 14:30:18 -0500

    Greedy Approach

---
+++ Arrays and Hashing/2870. Min Number of Operations to Make Array Empty.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        occ = dict()
+        for i in range(len(nums)):
+            if nums[i] in occ:
+                occ[nums[i]] += 1
+            else:
+                occ[nums[i]] = 1
+        res = 0
+        for elem, val in occ.items():
+            if val == 1:
+                return -1
+            if val % 3 == 0:
+                res += val // 3
+            else:
+                res += val // 3 + 1
+        
+        return res
+
+            
